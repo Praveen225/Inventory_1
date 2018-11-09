aad476ec946409669bc2bc2c109f88396ce72382
jpoornimaprasanna <codingpoornima@gmail.com>
2018-11-09 07:59:12 +0000

    changes in emp_notification

--- webapp/controller/EmployeePage.controller.js
+++ webapp/controller/EmployeePage.controller.js
@@ -6,21 +6,31 @@ sap.ui.define([
 
 	return Controller.extend("inventory.Inventory.controller.EmployeePage", {
 
-		/**
-		 * Called when a controller is instantiated and its View controls (if available) are already created.
-		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf inventory.Inventory.view.EmployeePage
-		 */
 		onInit: function () {
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.getRoute("EmployeePage").attachMatched(this._onObjectMatched, this);
+			/*var desig = this.getView().byId("desig").getValue();
+			if(desig === "Team Lead"){
+				this.getView().byId("bell").setVisible(true);
+			}*/
 		},
+		/*onAfterRendering: function(){
+		var desig = this.getView().byId("desig").getValue();
+			if(desig === "Team Lead"){
+				this.getView().byId("bell").setVisible(true);
+			}	
+		},*/
 
 		_onObjectMatched: function (oEvent) {
 			var oArg = oEvent.getParameters("arguments");
 			var oView = this.getView();
 			oView.setModel(this.getOwnerComponent().getModel("data"));
 			oView.bindElement("/empInfo/" + oArg.arguments.obj);
+			var are = oArg.arguments.obj;
+			var designation = this.getView().getModel("data").getProperty("/empInfo/" + are + "/designation/");
+			if (designation === "Team Lead") {
+				this.getView().byId("bell").setVisible(true);
+			}
 		},
 		onPressStatus: function (oEvent) {
 				var id = this.getView().byId("Id").getProperty("text");
@@ -41,15 +51,27 @@ sap.ui.define([
 			var empDescription = this.byId("desId").getValue();
 			var id = this.getView().byId("Id").getProperty("text");
 			var ticketNo = Math.floor((Math.random() * 10000000000) + 1);
-
+			var desig = this.getView().byId("desig").getProperty("text");
+			var empName = this.getView().byId("name").getProperty("text");
+			var inprocess="sap-icon://status-in-process";
+			var waiting="sap-icon://lateness";
+			var TlInprocess="Tl Inprocess";
+			var HRWaiting="HR Waiting";
 			this.obj = {
 				issue: this.comboValue,
 				description: empDescription,
 				Date: this.datePicker,
 				id: id,
-				ticketNo: ticketNo
+				name: empName,
+				Designation: desig,
+				ticketNo: ticketNo,
+				tlInprocess:inprocess,
+                tlInprocessText:TlInprocess,
+                hrWaitins:waiting,
+               hrWaitingText:HRWaiting
 			};
 			var myArray = [];
+			var newData = [];
 			var oModel = this.getView().getModel("data");
 			var oData = this.getView().getModel("data").oData;
 			for (var i = 0; i < oData.status[0][id].length; i++) {
@@ -57,11 +79,17 @@ sap.ui.define([
 			}
 			if (empDescription !== "" && this.comboValue !== "" && this.datePicker !== "") {
 				myArray.push(this.obj);
-				/*console.log(oModel.getProperty("/status/0"));*/
 				oModel.setProperty("/status/0/" + id, myArray);
 				myArray = oModel.getProperty("/status/0/" + id, myArray);
 				oModel.setProperty("/status/0/" + id, myArray);
 				MessageToast.show("Your Complaint Has Been Received");
+				/*var oData = this.getView().getModel("data").oData;*/
+				var allData = this.getView().getModel("data").oData.allData;
+				for (var j = 0; j < oData.allData.length; j++) {
+				newData.push(oData.allData[j]);
+				}
+				newData.unshift(this.obj);
+				this.getView().getModel("data").setProperty("/allData/", newData);
 			}
 			if (empDescription == "" && this.comboValue == "" && this.datePicker == "") {
 				MessageToast.show("Please enter all the Feilds");
@@ -73,9 +101,24 @@ sap.ui.define([
 		handleChange: function (oEvent) {
 			this.datePicker = oEvent.getParameters().value;
 		},
-		onLogout : function(){
-				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-					oRouter.navTo("login");	
+		onLogout: function () {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("login");
+		},
+		empNotification: function () {
+			/*var path = this.getView().getBindingContext().sPath.substring();
+			var index = path.substring(path.lastIndexOf('/') + 1);
+			var oModel = this.getView().getParent().getModel("data").oData.empInfo;
+			for (var i = 0; i < oModel.length; i++) {
+				if (oModel[i].id === index) {
+					var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+					oRouter.navTo("emp_Notification", {
+						obj: i
+					});
+				}
+			}*/
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("emp_Notification");
 		}
 
 	});
---
+++ webapp/controller/emp_Notification.controller.js
@@ -0,0 +1,93 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/m/MessageToast"
+], function (Controller, JSONModel, Filter, FilterOperator, MessageToast) {
+	"use strict";
+
+	return Controller.extend("inventory.Inventory.controller.emp_Notification", {
+
+		onInit: function () {
+			/*this.getView().setModel(new JSONModel(), "data");*/
+			this.getView().byId("emptyId").setVisible(true);
+			this.getView().byId("SimpleFormDisplay").setVisible(false);
+		},
+
+		onReject: function () {
+			var oView = this.getView();
+			var oDialog = oView.byId("idHrDialog");
+			if (!oDialog) {
+				oDialog = sap.ui.xmlfragment(oView.getId(), "inventory.Inventory.view.hrRejectDescription", this);
+				oView.addDependent(oDialog);
+			}
+			oDialog.open();
+		},
+		onHrRejectClose: function () {
+			this.getView().byId("idHrDialog").close();
+		},
+		onClick: function (oEvent) {
+			var path = this.getView().getBindingContext().sPath.substring();
+			var index = path.substring(path.lastIndexOf('/') + 1);
+			var oModel = this.getView().getParent().getModel("data").oData.empInfo;
+			for (var i = 0; i < oModel.length; i++) {
+				if (oModel[i].id === index) {
+					var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+					oRouter.navTo("EmployeePage", {
+						obj: i
+					});
+				}
+			}
+
+		},
+		onLogout: function () {
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("login");
+		},
+		onItemPressed: function (oEvent) {
+			/*this.getView().setModel(new JSONModel(), "data");*/
+			this.getView().byId("emptyId").setVisible(false);
+			this.getView().byId("SimpleFormDisplay").setVisible(true);
+			this.obj = oEvent.getParameters().listItem.getBindingContext("data").getObject();
+			var oDetails = this.getView().getModel("data");
+			oDetails.setProperty("/odata", this.obj);
+		},
+		onSearch: function (oEvent) {
+			var olist = this.getView().byId("list1"),
+				arr = [],
+				binding,
+				filters;
+			filters = new Filter({
+				filters: [new Filter("ticketNo", FilterOperator.Contains, oEvent.getSource().getValue()),
+					new Filter("Date", FilterOperator.Contains, oEvent.getSource().getValue())
+				],
+				and: false
+			});
+			binding = olist.getBinding("items");
+			arr.push(filters);
+			binding.filter(arr);
+			binding.filter(arr);
+		},
+		onApproved: function () {
+			var tcNo = this.obj.ticketNo;
+			var id = this.obj.id;
+			var ctid = this.getView().getModel("data").getProperty("/status/0/" + id);
+			for (var i = 0; i < ctid.length; i++) {
+				if (ctid[i].ticketNo === tcNo) {
+
+					this.getView().getModel("data").setProperty("/status/0/" + id + "/" + i + "/tlInprocess", "sap-icon://accept");
+					this.getView().getModel("data").setProperty("/status/0/" + id + "/" + i + "/tlInprocessText", "TL Accepted");
+					this.getView().getModel("data").setProperty("/status/0/" + id + "/" + i + "/hrWaitins", "sap-icon://status-in-process");
+					this.getView().getModel("data").setProperty("/status/0/" + id + "/" + i + "/hrWaitingText", "HR Inprocess");
+				}
+			}
+			/*if(){
+				
+			}*/
+
+		}
+
+	});
+
+});
